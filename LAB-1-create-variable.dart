6e5efc38d2014e63d8efc911dcac74b4d5429e58
Nardos <139006428+SABIYOO@users.noreply.github.com>
2024-03-08 15:21:49 +0300

    Create LAB-1-create-variable.dart

---
+++ LAB-1-create-variable.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Nardos Seyfu";
+  int age = 20;
+  String favoriteColor = "grey";
+
+  print("My name is $name.");
+  print("I am $age years old.");
+  print("My favorite color is $favoriteColor.");
+}
